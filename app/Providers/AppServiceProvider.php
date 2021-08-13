b793710792318cbf78df33f1cbd1e3ab3a7fc304
nicholasliversage <nicolasliversage@gmail.com>
2021-08-13 14:08:04 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        if(env('APP_ENV') !== 'local') { URL::forceScheme('https'); }
+       if(env('APP_ENV') !== 'local') { URL::forceScheme('https'); }
     }
 }
