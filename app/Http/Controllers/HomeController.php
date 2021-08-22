15a3ebdcf471dd1ccc08f77f66ba773f5177bb1e
nicholasliversage <nicolasliversage@gmail.com>
2021-08-22 09:46:06 +0200

    blogs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Room;
 use App\Models\Dishes;
+use App\Models\Blogs;
 
 class HomeController extends Controller
 {
@@ -12,7 +13,8 @@ class HomeController extends Controller
     public function index(){
         $rooms = Room::all();
         $dishes = Dishes::all();
-        return view('pages.index', compact('rooms','dishes'));
+        $blogs = Blogs::all();
+        return view('pages.index', compact('rooms','dishes','blogs'));
     }
 }
 
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -440,48 +440,26 @@ data=$('#myform').serialize();
       </div>
     </div>
     <div class="row d-flex">
+
+      @foreach ($blogs as $blog)
       <div class="col-md-4 d-flex ftco-animate">
         <div class="blog-entry align-self-stretch">
-          <a href="blog-single.html" class="block-20 rounded" style="background-image: url('images/event-1.jpg');">
+          <a href="/blogs/show/{{ $blog->id }}" class="block-20 rounded" style="background-image: url('{{ $blog->img1 }}');">
           </a>
           <div class="text mt-3 text-center">
               <div class="meta mb-2">
-              <div><a>December 25, 2019</a></div>
-              <div><a>Christmas Buffet</a></div>
-              <div><a  class="meta-chat"><span class="icon-chat"></span> 3</a></div>
+              <div><a>{{ $blog->created_at }}</a></div>
+              
+              <div><a  class="meta-chat"><span class="icon-chat"></span> {{ $blog->comments->count() }}</a></div>
             </div>
-            <h3 class="heading"><a href="blog-single.html">Christmas Buffet at Albatroz</a></h3>
+            <h3 class="heading"><a href="/blogs/show/{{ $blog->id }}">{{ $blog->title }}</a></h3>
           </div>
         </div>
       </div>
-      <div class="col-md-4 d-flex ftco-animate">
-        <div class="blog-entry align-self-stretch">
-          <a href="blog-single.html" class="block-20 rounded" style="background-image: url('images/image_2.jpg');">
-          </a>
-          <div class="text mt-3 text-center">
-              <div class="meta mb-2">
-              <div><a href="#">Oct. 30, 2019</a></div>
-              <div><a href="#">Admin</a></div>
-              <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-            </div>
-            <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 d-flex ftco-animate">
-        <div class="blog-entry align-self-stretch">
-          <a href="blog-single.html" class="block-20 rounded" style="background-image: url('images/image_3.jpg');">
-          </a>
-          <div class="text mt-3 text-center">
-              <div class="meta mb-2">
-              <div><a href="#">Oct. 30, 2019</a></div>
-              <div><a href="#">Admin</a></div>
-              <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-            </div>
-            <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
-          </div>
-        </div>
-      </div>
+      @endforeach
+      
+      
+      
     </div>
   </div>
 </section>
